63190f149229c35edc5744f29adec9b1cf44c883
Anchal-Mittal <32476930+Anchal-Mittal@users.noreply.github.com>
2017-10-05 11:56:34 +0530

    create pointer and apply operations on it

---
+++ pointerDataType.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int num = 7 ;  
+	//DECLARTION OF POINTERS
+int *ptr1 ,*ptr ;
+int arr[5],max, inc=0;	
+	//ASSIGNING THE ADDRESS TO THE POINTER
+	
+ptr = &num;
+ptr = NULL;	
+cout << " THE VALUE STORED AT ptr = " << *ptr << endl;	
+	
+	//NULL POINTER 
+
+cout << " THE VALUE STORED AT ptr1 = " << *ptr1 << endl;	
+	
+ptr1 = ptr;
+cout << " THE VALUE STORED AT ptr1 = " << *ptr1 << endl;	
+
+ptr1=arr;
+max=sizeof(arr)/sizeof(*arr);
+while(inc<max){
+arr[inc]=inc;
+
+cout << " THE VALUE STORED OF arr [ " << inc << " ] = " << arr[inc] << endl;	
+	
+cout << " THE VALUE STORED AT ptr1 = " << ptr1 << endl;	
+		
+inc++;
+ptr1++;	
+}
+	return 0;
+}
