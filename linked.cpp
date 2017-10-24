4bee303fb25443af59f07223d0d508bb66dbbaf1
Shivam Agarwal <24.shivamagarwal@gmail.com>
2017-10-24 22:14:04 +0530

    correction in insertion and some correction in class def

--- linked.cpp
+++ linked.cpp
@@ -1,25 +1,38 @@
-#include<iostream.h>
+#include<iostream>
+using namespace std;
 class Node{
+	public:
   int ele; 
   Node *next;
-  Node(int e , Node * n =0){
+  Node(int e , Node * n =NULL){
 	ele = e;
 	next = n;
   }
-}
+};
 
 class Link{
 Node *head;
 Node *tail;
+ public:
  Link(){
   head =0;
   tail= 0;
   }
 void insertion(int e);
+void display();
+void deletion(int el);
+};
+void Link :: display(){
+	Node *p=head;
+	cout<<"Content of linked list\n";
+	while(p != tail){
+		cout<<p->ele<<" ";
+		p=p->next;
+	}
+		cout<<p->ele;
 }
-
 void Link :: insertion(int e){
-	Node nd= new Node(e);
+	Node *nd= new Node(e);
 	if(head == NULL){
 		head= tail= nd;
 	}
@@ -29,9 +42,14 @@ void Link :: insertion(int e){
 	}
 		
 }
+
+
 int main()
 {
 Link n;
 n.insertion(5);
+n.insertion(6);
+n.insertion(6);
+n.display();
 
 }
