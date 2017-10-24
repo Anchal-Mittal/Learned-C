b73393b1d7a4c53c16ed6e5dee7573cd935c5841
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-10-25 03:03:33 +0530

    Declare a global variable and local variable and  perform operation using :: operator

--- globalVariabeLocalScope.cpp
+++ globalVariabeLocalScope.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 int var =10;
 
+//typedef int INTEGER = 12;** ERROR CAN NOT INITAILIZE HERE
+
 int main(){
 int var =  1;
 cout <<"THE SCOPE OF LOCAL VARIABEL ";
 cout << var << endl;	
+
+  // ::var = :: var + var;**ERROR CAN ASSIGNED VALUE TO A VARIABLE WITH SCOPE RESOLUTION OPERATOR
+  
 cout << "THE SCOPE OF GLOBAL VARIABLE ";
-cout << :: var;
+cout << :: var << endl;
+var = :: var + var; // CAN USE :: OPERATOR HERE
+cout <<"THE SCOPE OF LOCAL VARIABEL ";
+cout << var << endl;
 return 0;	
 }
