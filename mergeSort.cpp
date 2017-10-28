ecc261fe8eb39233e67d29092221de01b4ebca16
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-10-29 00:15:03 +0530

    Create an array and sort it using merge sort

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,79 +1,95 @@
-#include<iostream.h>
-#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+           // DECLARATION OF FUNCTIONS
 void merge(int a[10],int low,int high);
 void mergesort(int a[10],int low,int mid,int high);
-int b[10];
-int main()
-{
-clrscr();
-int a[10],i,j,n;
-cout<<"ENTER THE SIZE OF ARRAY"<<endl;
-cin>>n;
-cout<<"ENTER THE ELEMENTS OF THE ARRAY"<<endl;
-for(i=1;i<=n;i++)
-{
-cin>>a[i];
-}
 
-merge(a,1,n);
-cout<<"SORTED ARRAY"<<endl;
-for(i=1;i<=n;i++)
-{
-cout<<a[i]<<" ";
-}
-getch();
+
+int b[10];
+
+        //EXECUTION WILL START FROM HERE
+int main(){
+
+int arr[10],i,j,sizeArray;
+
+ cout<<"ENTER THE NUMBER OF ELEMENTS IN ARRAY"<<endl;
+  cin>>noOfElement;
+
+ cout<<"ENTER THE ELEMENTS OF THE ARRAY"<<endl;
+ for (i=0; i<noOfElement; i++){
+   cin>>arr[i];
+   }
+
+        // CALLING MERGEFUNCTION 
+merge(arr,1,noOfElement);
+
+ cout<<"SORTED ARRAY"<<endl;
+  for (i=1;i<=noOfElement;i++){
+    cout<<a[i]<<" "; 
+    }
+
 return 0;
 }
-void merge (int a[10],int low,int high)
+
+
+              // DEFINATION OF MERGESORT FUNCTION
+void merge (int arr[10],int low,int high)
 {
  int mid;
-if(low<high)
-{
-mid=(low+high)/2;
-merge(a,low,mid);
-merge(a,(mid+1),high);
-mergesort(a,low,mid,high);
-}
+ 
+  if(low<high){
+     mid=(low+high)/2; 
+     merge(arr,low,mid);
+     merge(arr,(mid+1),high);
+     mergesort(arr,low,mid,high);
+   }
+ 
 return;
 }
- void mergesort(int a[10],int low,int mid,int high)
+ 
+               //SORTING OCCURS HERE 
+void mergesort(int arr[],int low,int mid,int high)
 {
 
 int i,j,k;
 i=low;
 k=low;
 j=mid+1;
-while((i<=mid)&&(j<=high))
-{
- if(a[i]<=a[j])
- {
- b[k]=a[i];
- k++;
- i++;
- }
- else
- {
- b[k]=a[j];
- k++;
- j++;
- }
+while ((i<=mid)&&(j<=high)){
+ 
+    if (a[i]<=a[j]) {
+       b[k]=a[i];
+       k++;
+       i++;
+       }
+    
+    else{
+       b[k]=a[j];
+       k++;
+       j++;
+       }
+ 
 }
 
-while(i<=mid)
-{
-b[k]=a[i];
-k++;
-i++;
+while (i <= mid){
+
+ b[k] = a[i];
+ k++;
+ i++;
 }
-while(j<=high)
-{
-b[k]=a[j];
-k++;
-j++;
-}
-for(i=low;i<=high;i++)
-{
-a[i]=b[i];
-}
+
+ while (j <= high){
+  
+   b[k]=a[j];
+   k++;
+   j++;
+  }
+
+ for (i=low;i<=high;i++){
+      a[i]=b[i];
+       }
+
 return ;
 }
