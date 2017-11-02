171dd5272f7983d55db42d96ac591ae05f881d41
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-11-02 13:08:24 +0530

    Create a program and use logical operator

--- logicalOperator.cpp
+++ logicalOperator.cpp
@@ -9,17 +9,29 @@ int main()
     int num3 =8;
     int num4 =8;
     
+            // AND OPERATOR
   if((num1==num2)&&(num3==num4)){
      cout << "num1 is equal to num2 and num3 is equal to num4" << endl;
    }
-         
-  if((num1==num2)||(num3 == num4)){
-     cout << "num1 is equal to num2 or num3 is equal to num4" << endl;
+   
+            // AND OPERATOR WILL CHECK BOTH CONDITIONS 
+   if((num1 == num2)&& (cout<< "num1 and num2 are equal" << endl));
+       
+            // OR OPERATOR
+   if((num1==num2)||(num3 == num4)){
+      cout << "num1 is equal to num2 or num3 is equal to num4" << endl;
+   }
+    
+            // WILL CHECK SECOND CONDITION IF FIRST ONE IS NOT TRUE
+   if((num1 == num2) ||(cout<< "num1 and num2 are not equal" << endl));
+   
+            // DOES NOT CHECK SECOND CONDITION IF FIRST IS TRUE
+   if((num3 == num4) ||(cout<< "num3 and num4 are not equal" << endl)); 
+   
+           // NOT  OPERATOR
+    if(!num1){
+     cout << " num1 is not zero" << endl;
    }
    
-  if(!num1){
-     cout << " num1 is not zero" << endl;
-  }
-   
     return 0;
-} 
+}
