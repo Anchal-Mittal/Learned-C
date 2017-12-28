146151867a0f8d82bb9065bd859879ace593cab0
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-12-28 22:45:16 +0530

    swap two numbers using * and /

---
+++ swap.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+//--------------------------------------------SWAP------------------------------------------------------------------------
+
+void swap(int &num1, int &num2){
+	num1=num1* num2;
+	num2=num1/num2;
+	num1=num1/num2;
+}
+
+//-----------------------------------------------MAIN FUNCTION----------------------------------------------------------
+int main(){
+	int num1,num2;
+	cout << "ENTER FIRST NUMBER " << endl;
+	cin >> num1;
+	cout << "ENTER TWO NUMBER "<< endl;
+	cin >> num2;
+	swap(num1,num2);
+	cout << "AFTER SWAPING \nFIRST  "<<num1 <<"\nSECOND "<< num2;
+	return 0;
+}
